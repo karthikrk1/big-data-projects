0c899d61292803199b9b646ff25cacd9a361563c
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-18 20:39:48 -0600

    delete target dir

--- sqoop-examples/sqoop_import_delete_target_dir.sh
+++ sqoop-examples/sqoop_import_delete_target_dir.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+sqoop import \
+ --connect jdbc:mysql://db.hostname:3306/db \
+ --username user \
+ -P \
+ --table table_name \
+ --warehouse-dir /user/karthik/sqoop_import/db \
+ --num-mappers 1 \
+ --delete-target-dir
