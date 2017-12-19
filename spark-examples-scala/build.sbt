0dc1363fa9fdb8ba95fa1017376b4bf7800f71ab
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-18 19:48:00 -0600

    WordCount in scala

--- spark-examples-scala/build.sbt
+++ spark-examples-scala/build.sbt
@@ -3,4 +3,6 @@ name := "spark-examples-scala"
 version := "1.0"
 
 scalaVersion := "2.10.4"
+
+libraryDependencies ++= Seq("org.apache.spark" %% "spark-core" % "1.6.3")
     
---
+++ spark-examples-scala/src/main/scala/WordCount.scala
@@ -0,0 +1,34 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+/**
+  * Created by Karthik Ramakrishnan on 12/18/17.
+  *
+  * This program is the Scala version of the WordCount program
+  * to be run in Apache Spark
+  *
+  * Assumptions:
+  *   This program assumes that the input file is delimited by space (" ")
+  *   If this is not the case then the delimiter should be used in line 22
+  *   inside flatMap
+  *
+  * Format to run:
+  *
+  * spark-submit --class WordCount --master [yarn/local] /path/to/jar <input-file> <output-file>
+  *  
+  */
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("Word Count")
+    val sc = new SparkContext(conf)
+    // This makes sure INFO doesn't get shown. But for learning purposes
+    // this step can be avoided to see how the job works in each step
+    sc.setLogLevel("WARN")
+    val inputFile = sc.textFile(args(0))
+    val count = inputFile.flatMap(line => line.split(" "))
+                            .map(word => (word,1))
+                              .reduceByKey(_+_)
+    count.saveAsTextFile(args(1))
+
+  }
+}
