8bc1dde3933e1502bbe69952f832c39eed45fd1f
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-20 21:08:30 -0600

    Import with delete-target-dir

--- sqoop-examples/sqoop_import_delete_target_dir.sh
+++ sqoop-examples/sqoop_import_delete_target_dir.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# This is another flavor of the sqoop import with the delete-target-dir
+# swtich. This switch is needed when we are importing the same table
+# within the warehouse-dir. The import will still fail if the warehouse-dir
+# already has another directory with the name of the table we are trying
+# to import. If so this switch can be enabled to delete the target dir
+# before importing to the location
 sqoop import \
  --connect jdbc:mysql://db.hostname:3306/db \
  --username user \
