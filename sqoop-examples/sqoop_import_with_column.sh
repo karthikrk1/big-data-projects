9e16f25c9ec23c38a8b4cd65b9fadfa6c6999f19
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-21 22:03:16 -0600

    column switch

---
+++ sqoop-examples/sqoop_import_with_column.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# This script uses the column switch. This is used along with the
+# --table switch. This is used to select only certain columns from the
+# table and not all. This is part of a transformation and filtering process.
+
+sqoop import \
+ --connect jdbc:mysql://db.hostname.com:3306 \
+ --username user \
+ -P \
+ --table table_name \
+ --column id,first_name,last_name \
+ --warehouse-dir /user/karthik/sqoop_import/db \
+ -m 2
+
