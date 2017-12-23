b3a3ac5d0241260161cbdca4b730ff67cf1d206e
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-22 22:02:51 -0600

    sqoop export with column mapping

---
+++ sqoop-examples/Sqoop_export/prep/sqoop_export_column_mapping_prep_1.sql
@@ -0,0 +1,4 @@
+-- Create a new MySQL table
+
+create table column_mapping_demo(order_date varchar(30), revenue float,
+desc varchar(200));
---
+++ sqoop-examples/Sqoop_export/sqoop_export_column_mapping.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# This introduces the --columns switch for Sqoop Export
+# This is used to export only certain columns from the hive/hdfs into MySQL
+# This is useful when some columns are NULLable 
+sqoop export \
+    --connect jdbc:mysql://db.hostname.com:3306/db \
+    --username user \
+    -P \
+    --export-dir /user/hive/warehouse/karthik_test/daily_revenue \
+    --input-fields-terminated-by "\001" \
+    --table column_mapping_demo \
+    --columns order_date, revenue
