244faa5ff9b7bb6cdbeae48ad7b81d90638ed240
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-23 23:41:24 -0600

    Some more samples

--- spark-examples-scala/spark_basic_commands.sh
+++ spark-examples-scala/spark_basic_commands.sh
@@ -8,4 +8,11 @@ spark-shell --master yarn --conf spark.ui.port=<any 5 digit portnumber>
 
 spark-shell --master yarn --conf spark.ui.port=10948
 
-# 
+# Initialize with custom num-executors and executor-memory
+# This is needed if we have less data to process and allocate more
+# or we have more data and allocate less memory
+
+spark-shell --master yarn \
+    --conf spark.ui.port=10948 \
+    --num-executors 1 \
+    --executor-memory 512M
---
+++ spark-examples-scala/src/main/scala/Sample.scala
@@ -0,0 +1,26 @@
+import org.apache.spark.{SparkConf, SparkContext}
+/**
+  * Created by Karthik Ramakrishnan on 12/23/17.
+  */
+object Sample {
+  val conf = new SparkConf().setAppName("Sample App").setMaster("yarn-client")
+  val sc = new SparkContext(conf)
+  // to check all the spark conf
+  // This can also be viewed in the tracking URL
+  // which is usually found when running the spark-shell
+  sc.getConf.getAll.foreach(println) // prints all conf line by line
+  // Creating a RDD from HDFS
+  val orders = sc.textFile("hdfs:///user/karthik/file")
+  // get the first record of this RDD
+  orders.first()
+  // get the first 10 records
+  orders.take(10)
+  // Reading from a local file system
+  // Read from local fs using Source.fromFile
+  // get the lines and make a list.
+  val raw = scala.io.Source.fromFile("/local/path").getLines.toList
+  // convert this to a RDD using sc.parallelize
+  val rawRDD = sc.parallelize(raw)
+  // Reading top 10 from raw
+  rawRDD.take(10)
+}
