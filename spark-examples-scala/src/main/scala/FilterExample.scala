58a602dcf85035fbc59cf3c23e380baffe009bd5
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-24 22:53:10 -0600

    Filter example

---
+++ spark-examples-scala/src/main/scala/FilterExample.scala
@@ -0,0 +1,43 @@
+import org.apache.spark.{SparkContext, SparkConf}
+/**
+  * Created by Karthik Ramakrishnan on 12/24/17.
+  * This example shows some filtering applied on the data
+  * using filter API of Spark
+  */
+object FilterExample {
+  val conf = new SparkConf().setAppName("Filter Example").setMaster("yarn-client")
+  val sc = new SparkContext(conf)
+
+  /*
+  The use case here is to filter the rows that have a status of
+  "COMPLETE" or "CLOSED" and having order_date in the month of August 2013
+   */
+
+  // Read a new RDD
+  val orders = sc.textFile("hdfs:///user/karthik/orders")
+
+  // Get the distinct order status
+  // This is needed in case there are some status synonymous to the
+  // states we need
+  // the fourth column in orders is order_status so we say array index 3
+  val distinctStatus = orders.map(order => order.split(",")(3)).distinct
+
+  // print and see each status
+  // usually for debugging
+  // collect should not be used on large datasets
+  distinctStatus.collect.foreach(println)
+
+  // Get the final result
+  // We are using a variable to make it easier
+  // The Boolean OR and AND are used to combine our use case
+  val result = orders.filter(order => {
+    val o = order.split(",")
+    (o(3) == "COMPLETE" || o(3) == "CLOSED") && (o(1).contains("2013-09"))
+  })
+
+  // See some stats
+  result.count()
+
+  result.take(10).foreach(println)
+  
+}
