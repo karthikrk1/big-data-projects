4ba1de3f35678397df8dad304d23d71d79e1ee46
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-25 22:59:26 -0600

    some comments

--- spark-examples-scala/src/main/scala/AggregationExample3.scala
+++ spark-examples-scala/src/main/scala/AggregationExample3.scala
@@ -7,6 +7,15 @@ object AggregationExample3 {
     .setMaster("yarn-client")
   val sc = new SparkContext(conf)
 
+  // groupByKey to group all the records by key
+  // Here we use groupByKey to group all order_items by its order_ids
+  // and perform sum to get per order total
+  /*
+  Notes:
+  groupByKey is not as performant as the other two variants
+  reduceByKey and aggregateByKey
+  since it does not use a combiner
+   */
   val orderItems = sc.textFile("hdfs:///user/karthik/order_items")
   val orderItemsMap = orderItems.map(oi => (oi.split(",")(1).toInt, oi.split(",")(4).toFloat))
   val orderItemsGBK = orderItemsMap.groupByKey
@@ -15,7 +24,12 @@ object AggregationExample3 {
   val perOrderRevenue = orderItemsGBK.map(r => (r._1, r._2.toList.sum))
 
   // get data in descending order by order_item_subtotal for each order_id
-
+  // this gives result as follows:
+  /*
+  (2, 100.0)
+  (2, 99.0)
+  and so on based on each key. 
+   */
   val descOrder = orderItemsGBK.flatMap(r => {
     r._2.toList.sortBy(o => -o).map(k=> (r._1, k))
   })
