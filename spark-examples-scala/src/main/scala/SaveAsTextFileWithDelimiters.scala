3ef56182b7e11f4c8d1fd2fea44c1d6cb855d748
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-26 22:29:13 -0600

    Saving with delimiters

--- spark-examples-scala/src/main/scala/SaveAsTextFileWithDelimiters.scala
+++ spark-examples-scala/src/main/scala/SaveAsTextFileWithDelimiters.scala
@@ -1,6 +1,32 @@
+import org.apache.spark.{SparkContext, SparkConf}
 /**
-  * Created by karthikrk on 12/26/17.
+  * Created by Karthik Ramakrishnan on 12/26/17.
   */
 object SaveAsTextFileWithDelimiters {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("saveAsTextFile with delimiters Example")
+      .setMaster("yarn-client")
 
+    val sc = new SparkContext(conf)
+
+    val orders = sc.textFile("hdfs:///user/karthik/orders")
+
+    // Using countByKey will give a Map instead of RDD
+    // and we cannot save
+    // so using reduceByKey to get a RDD on which we can save
+
+    val countByStatus = orders.map(order => (order.split(",")(3),1)).
+      reduceByKey((total, count) => total+count)
+
+    // use a map phase to convert with a delimter and then call save
+    countByStatus.map(r => r._1 + "\t" + r._2).
+      saveAsTextFile("/user/karthik/order_count_example_delim")
+
+    // validating => read it back using sc.textFile and print sample records
+
+    // Note: collect should not be used on huge datasets
+    sc.textFile("/user/karthik/order_count_example_delim").
+      collect.
+      foreach(println)
+  }
 }
