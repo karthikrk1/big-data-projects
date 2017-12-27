fe60444c971abf463508abb1a28c8484eea99a00
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-26 22:36:18 -0600

    Saving in standard file formats

--- spark-examples-scala/src/main/scala/SaveInStandardFormats.scala
+++ spark-examples-scala/src/main/scala/SaveInStandardFormats.scala
@@ -1,6 +1,27 @@
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.sql.SQLContext
 /**
-  * Created by karthikrk on 12/26/17.
+  * Created by Karthik Ramakrishnan on 12/26/17.
   */
 class SaveInStandardFormats {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("Save in standard formats Example")
+      .setMaster("yarn-client")
 
+    val sc = new SparkContext(conf)
+
+    val sqlContext = new SQLContext(sc)
+
+    // Standard formats include:
+    // json, orc, parquet and avro
+
+    // Using save and load API
+    val ordersDF = sqlContext.read.json("/user/krk_json/orders")
+
+    // save needs the type as argument
+    ordersDF.save("/user/karthik/orders_parquet", "parquet")
+
+    // load for validating using show method
+    sqlContext.load("/user/karthik/orders_parquet", "parquet").show
+  }
 }
