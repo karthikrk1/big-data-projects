fdece6fa41c36eaa379f61c8f527571932d7b13c
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-27 22:27:13 -0600

    some date functions

---
+++ spark-examples-scala/src/hive_db/some_date_fns.sql
@@ -0,0 +1,19 @@
+select current_date;
+
+select current_timestamp;
+
+select date_add(current_date, 10);
+
+select date_format(current_date, 'y');
+
+select day('2017-06-30');
+
+select to_date(current_timestamp);
+
+select to_unix_timestamp(current_timestamp);
+
+select to_utc_timestamp(current_timestamp, "CST");
+
+
+
+
