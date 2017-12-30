79a3000727548513b50bfe7c208eb8f64751f6ec
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-30 17:50:04 -0600

    Flume and spark

--- spark-streaming-scala/src/main/scala/StreamingDeptCountFlume.scala
+++ spark-streaming-scala/src/main/scala/StreamingDeptCountFlume.scala
@@ -1,16 +1,21 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{StreamingContext, Seconds}
+import org.apache.spark.streaming.flume._
 /**
   * Created by Karthik Ramakrishnan on 12/30/17.
   */
 object StreamingDeptCountFlume {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("streaming dept count")
+    val conf = new SparkConf().setAppName("flume streaming dept count")
       .setMaster(args(0))
 
     val ssc = new StreamingContext(conf, Seconds(30))
 
-    val messages = ssc.socketTextStream(args(1), args(2).toInt)
+    val stream  = FlumeUtils.createPollingStream(ssc, args(1), args(2).toInt)
+
+    val messages = stream.map(st => {
+      new String(st.event.getBody.array())
+    })
 
     val departmentMessages = messages.
       filter(msg => {
@@ -27,7 +32,7 @@ object StreamingDeptCountFlume {
 
     val departmentTraffic = departments.reduceByKey(_+_)
 
-    departmentTraffic.saveAsTextFiles("/user/karthikramakrishnan20/deptwisetraffic/cnt",".txt")
+    departmentTraffic.saveAsTextFiles("/user/karthikramakrishnan20/sdcflume/cnt")
 
     ssc.start()
     ssc.awaitTermination()
