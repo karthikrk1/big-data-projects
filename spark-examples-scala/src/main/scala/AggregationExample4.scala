61e4bbc4a3f8f445614c3bf18073291d98e7b58b
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-30 22:18:03 -0600

    arguments...

--- spark-examples-scala/src/main/scala/AggregationExample4.scala
+++ spark-examples-scala/src/main/scala/AggregationExample4.scala
@@ -5,7 +5,7 @@ import org.apache.spark.{SparkContext, SparkConf}
 object AggregationExample4 {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("Aggregation Example with reduceByKey")
-      .setMaster("yarn-client")
+      .setMaster(args(0))
     val sc = new SparkContext(conf)
 
     // using reduce By key to get the total
@@ -13,7 +13,7 @@ object AggregationExample4 {
     // as shown in the sum where both intermediate and final results
     // depend on the same sum logic
     // this can also be used to get the min and max as shown below
-    val orderItems = sc.textFile("hdfs:///user/karthik/order_items")
+    val orderItems = sc.textFile(args(1))
     val orderItemsMap = orderItems.map(oi => (oi.split(",")(1).toInt, oi.split(",")(4).toFloat))
     val revenuePerOrder = orderItemsMap.reduceByKey((total, revenue) => total + revenue)
     val minRevenuePerOrder = orderItemsMap.reduceByKey((min, revenue) => if(min < revenue) min  else revenue)
