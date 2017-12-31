91484192b1d4a13ef744993e67c56ed97c21d527
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-30 22:16:25 -0600

    agruments...

--- spark-examples-scala/src/main/scala/AggregationExample1.scala
+++ spark-examples-scala/src/main/scala/AggregationExample1.scala
@@ -5,14 +5,14 @@ import org.apache.spark.{SparkContext, SparkConf}
 object AggregationExample1 {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("Aggregation Example with countByKey")
-      .setMaster("yarn-client")
+      .setMaster(args(0))
     val sc = new SparkContext(conf)
 
     // extract order status and count by key
     // order status is the fourth column and hence array index 3
     // Schema:
     // order_id, customer_id, order_date, order_status
-    val orders = sc.textFile("hdfs:///user/karthik/orders")
+    val orders = sc.textFile(args(1))
 
     val ordersMap = orders.map(order => (order.split(",")(3),1)).countByKey
 
