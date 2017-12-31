6d785c1e3ff851e7af51e971baf32df71e4ac827
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-12-30 22:19:43 -0600

    arguments...

--- spark-examples-scala/src/main/scala/DataFrameExample.scala
+++ spark-examples-scala/src/main/scala/DataFrameExample.scala
@@ -5,14 +5,14 @@ import org.apache.spark.sql.SQLContext
   */
 object DataFrameExample {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("Sample DataFrame App").setMaster("yarn-client")
+    val conf = new SparkConf().setAppName("Sample DataFrame App").setMaster(args(0))
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
     // Create a DF using read API
     // Here example shows a JSON file
     // Other supported formats include Avro, ORC and Parquet
-    val exDF = sqlContext.read.json("hdfs:///user/karthik/file.json")
+    val exDF = sqlContext.read.json(args(1))
     // preview the data
     exDF.show
     // Schema for DF
@@ -22,7 +22,7 @@ object DataFrameExample {
     // another API to create DF
     // load needs another argument to specify type
     // read has separate methods for each type.
-    val anotherDF = sqlContext.load("hdfs:///user/karthik/file.json","json")
+    val anotherDF = sqlContext.load(args(2))
     // same operations can be applied
     // for example showing some preview data
     anotherDF.show
